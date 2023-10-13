b323477c4ba133b17e63b296aeecc5f78c2d675e
SaiSrujan-Asara <sunnysaisrujan@gmail.com>
2023-10-14 01:33:20 +0530

    added

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,1 +1,19 @@
-[ "`git log --pretty=%H ...refs/heads/main^ | head -n 1`" = "`git ls-remote origin -h refs/heads/main |cut -f1`" ] && echo "up to date" || echo "not up to date"
+#!/bin/bash
+GITNAME="terraform"
+
+
+if [ "`git log --pretty=%H ...refs/heads/main^ | head -n 1`" = "`git ls-remote origin -h refs/heads/main |cut -f1`" ] ; then
+    status=0
+    statustxt="up to date"
+else
+    status=2
+    statustxt="not up to date"
+fi
+
+if [[ `git status --porcelain` ]]; then
+    status=1
+    statustxt="uncommited"
+fi
+
+
+echo "$status git_status_$GITNAME - $statustxt"
