51073585ea05bd64c028ab2ba1aec8c5367de950
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-10-13 17:58:24 +0530

    Update and rename azure-pipelines.yaml to githubbranch.sh

---
+++ pipeline/githubbranch.sh
@@ -0,0 +1,12 @@
+# Get the latest commit hash of the main branch
+    main_branch_commit=$(git ls-remote --heads origin main | awk '{print $1}')
+
+    # Get the latest commit hash of the current feature branch
+    feature_branch_commit=$(git rev-parse HEAD)
+
+    # Compare the commit hashes
+    if [ "$main_branch_commit" != "$feature_branch_commit" ]; then
+      echo "The feature branch is outdated with the main branch."
+      echo "Main branch commit: $main_branch_commit"
+      echo "Feature branch commit: $feature_branch_commit"
+      exit 1  # Fail the pipeline
