9dc76085c0c5df20d6782159e29acb0a01d5ded6
SaiSrujan-Asara <sunnysaisrujan@gmail.com>
2023-10-13 23:19:42 +0530

    added

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
+# Define the names of the main and feature branches
 main_branch="main"
 feature_branch="test1"
 
 # Fetch the latest changes from the remote repository
-git fetch origin
+git fetch
 
 # Check if the feature branch is up to date with the main branch
-if git rev-list --left-right --count origin/${main_branch}...origin/${feature_branch} | awk '{print $1}' | grep -qE "^[1-9]"; then
+if git log --graph --oneline --decorate ${main_branch}..${feature_branch} --abbrev-commit | grep -qE "^\*"; then
   echo "Feature branch is not up to date with the main branch."
   exit 1
 else
   echo "Feature branch is up to date with the main branch."
   exit 0
 fi
-
-
-# # Get the latest commit hash of the main branch
-# main_branch_commit=$(git ls-remote --heads origin main | awk '{print $1}')
-
-# # Get the latest commit hash of the current feature branch
-# feature_branch_commit=$(git rev-parse HEAD)
-
-# # Compare the commit hashes
-# if [ "$main_branch_commit" != "$feature_branch_commit" ]; then
-#   echo "The feature branch is outdated with the main branch."
-#   echo "Main branch commit: $main_branch_commit"
-#   echo "Feature branch commit: $feature_branch_commit"
-#   exit 1  # Fail the pipeline
-# else
-#   echo "Feature branch is up to date with the main branch."
-#   exit 0
-# fi
