767fab7863e901c366a5ea1c78ac52f2afc08328
SaiSrujan-Asara <sunnysaisrujan@gmail.com>
2023-10-18 22:54:54 +0530

    added

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -6,13 +6,19 @@ BRANCH_NAME=$(basename $CURRENT_BRANCH)
 
 echo "Current Branch: $BRANCH_NAME"
 
+# Set up GitHub credentials
+export GITHUB_PAT=$GITHUB_PAT
+
+git config --global user.email "your.email@example.com"
+git config --global user.name "Your Name"
+
+# Clone the GitHub repository using the PAT
+git clone https://$GITHUB_PAT@github.com/SaiSrujan-Asara/terraform.git  
+
+# Continue with your existing logic
 git checkout "$BRANCH_NAME"
-
-# Fetch the latest changes from the remote main branch
 git fetch origin main
-
-# Use `git rev-list` to count the number of commits the  remote main branch is ahead of feature branch 
-commits_ahead=$(git rev-list --count "$feature_branch"..origin/main)
+commits_ahead=$(git rev-list --count "$BRANCH_NAME"..origin/main)
 
 if [ "$commits_ahead" -eq 0 ]; then
     echo "Feature branch is up to date with the main branch."
