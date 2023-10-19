b37afa4b85d2f31740a561fd863b76ac40f04139
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-10-19 17:07:01 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -23,6 +23,7 @@ commits_ahead=$(git rev-list --count "$BRANCH_NAME"..origin/main)
 if [ "$commits_ahead" -eq 0 ]; then
     echo "Feature branch is up to date with the main branch."
 else
+    exit 1
     echo "Main branch is $commits_ahead commits ahead of the Feature branch."
     echo "Please update your feature branch with the main branch"
 fi
