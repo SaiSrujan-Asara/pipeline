f40f542e30070b479085de95856c5f32e9649239
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-10-25 00:43:35 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,35 +1,8 @@
-#!/bin/bash
-
-# CURRENT_BRANCH=$1
-
-# BRANCH_NAME=$(basename $CURRENT_BRANCH)
-
-# echo "Current Branch: $(Build.SourceBranch)."
-
-# Set up GitHub credentials
-# export GITHUB_PAT=$GITHUB_PAT
-
-# git config --global user.email "your.email@example.com"
-# git config --global user.name "Your Name."
-
-# Clone the GitHub repository using the PAT
-# git clone https://$GITHUB_PAT@github.com/SaiSrujan-Asara/terraform.git  
-
-# Continue with your existing logic
-# git checkout "$BRANCH_NAME"
-
-# BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-# Print the current branch name
-# echo "Current Branch: $BRANCH_NAME"
-
 branch_reference="$BUILD_SOURCEBRANCHNAME"
 branch_name=${branch_reference#refs/heads/}
 echo "Branch Name: $branch_name"
-
 git fetch
-
 commits_ahead=$(git rev-list --count origin/main ^origin/"$branch_name")
-
 if [ "$commits_ahead" -eq 0 ]; then
     echo "Feature branch is up to date with the main branch."
 else
