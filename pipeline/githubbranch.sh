79b3c409e33f8fc13bf8bda5964dd88df180b470
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-10-26 23:03:07 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,18 +1,11 @@
-check_branch_status() {
-  echo "Branch Name: $BUILD_SOURCEBRANCHNAME"
-  commits_ahead=$(git rev-list --count origin/main ^origin/"$branch_name")
-  commits_bhead=$(git rev-list --count ^origin/main origin/"$branch_name")
-  echo "Missing Main commits in feature branch : $commits_ahead"
-  echo "Missing feature branch commits in Main branch : $commits_bhead"
-  if [ "$commits_ahead" -eq 0 ]; then
-      echo "Feature branch is up to date with the main branch."
-  else
-      echo "Please update your feature branch with the main branch."
-      exit 1
-  fi
-}
-if [ "$BUILD_SOURCEBRANCHNAME" != "main" ]; then
-  check_branch_status
+echo "Branch Name: $BUILD_SOURCEBRANCHNAME"
+commits_ahead=$(git rev-list --count origin/main ^origin/"$branch_name")
+commits_bhead=$(git rev-list --count ^origin/main origin/"$branch_name")
+echo "Missing Main commits in feature branch : $commits_ahead"
+echo "Missing feature branch commits in Main branch : $commits_bhead"
+if [ "$commits_ahead" -eq 0 ]; then
+    echo "Feature branch is up to date with the main branch."
 else
-  echo "Branch is 'main'. No need to check for updates."
+    echo "Please update your feature branch with the main branch."
+    exit 1
 fi
