d6d0bed4fd33a4eaf9fc76d3f6035820ef6d12ad
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-10-26 19:55:34 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -2,8 +2,10 @@ branch_reference="$BUILD_SOURCEBRANCHNAME"
 branch_name=${branch_reference#refs/heads/}
 echo "Branch Name: $branch_name"
 # git fetch
-commits_ahead=$(git rev-list --count origin/main ^origin/"$branch_name")
-echo "total commits: $commits_ahead"
+commits_ahead=$(git rev-list --count origin/main ^origin/feat/"$branch_name")
+commits_bhead=$(git rev-list --count ^origin/main origin/feat/"$branch_name")
+echo "Missing Main commits in feature branch : $commits_ahead"
+echo "Missing feature branch commits in Main branch : $commits_bhead"
 if [ "$commits_ahead" -eq 0 ]; then
     echo "Feature branch is up to date with the main branch."
 else
