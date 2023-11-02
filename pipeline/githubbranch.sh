b70913ac9413d17a606fd2e66542869d8106c5b0
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-11-02 19:19:23 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,8 +1,8 @@
 branch_name="$BUILD_SOURCEBRANCHNAME"
 echo "Branch Name: $branch_name"
 # git fetch
-commits_ahead=$(git rev-list --count origin/main ^origin/(feat|test|fix|bug)/"$branch_name")
-commits_bhead=$(git rev-list --count ^origin/main origin/(feat|test|fix|bug)/"$branch_name")
+commits_ahead=$(git rev-list --count origin/main "^origin/\(feat\|test\|fix\|bug\)/$branch_name")
+commits_bhead=$(git rev-list --count "^origin/main" "origin/\(feat\|test\|fix\|bug\)/$branch_name")
 echo "Missing Main commits in feature branch : $commits_ahead"
 echo "Missing feature branch commits in Main branch : $commits_bhead"
 if [ "$commits_ahead" -eq 0 ]; then
