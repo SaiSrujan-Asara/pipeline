f74d49622c9aa07d9ec7e7efe7f800a40e02e642
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2023-11-02 19:59:25 +0530

    Update githubbranch.sh

--- pipeline/githubbranch.sh
+++ pipeline/githubbranch.sh
@@ -1,6 +1,10 @@
 branch_name="$BUILD_SOURCEBRANCHNAME"
 echo "Branch Name: $branch_name"
 # git fetch
+full_branch_name=$(echo "$(Build.SourceBranch)" | cut -d'/' -f 2)
+# Output the full branch name
+echo "Full Branch Name: $full_branch_name"
+
 commits_ahead=$(git rev-list --count origin/main ^origin/feat/"$branch_name")
 commits_bhead=$(git rev-list --count ^origin/main origin/"$branch_name")
 echo "Missing Main commits in feature branch : $commits_ahead"
